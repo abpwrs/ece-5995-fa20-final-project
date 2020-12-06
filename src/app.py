722c7a4cd0f746d84d0cfd6d6d2c5428c25c061a
abpwrs <alexander-powers@uiowa.edu>
2020-12-06 13:30:54 -0600

    format w/ black and isort

--- src/app.py
+++ src/app.py
@@ -1,15 +1,14 @@
+import mongoengine as me
 from flask import Flask, jsonify, render_template
+from flask_assets import Bundle, Environment
+
 # from flask.ext.scss import Scss
 from flask_mongoengine import MongoEngine
-import mongoengine as me
-from flask_assets import Environment, Bundle
-
-
 
 # init app
 app = Flask(__name__)
 # config mongodb
-app.config['MONGODB_SETTINGS'] = {
+app.config["MONGODB_SETTINGS"] = {
     "db": "covid",
 }
 
@@ -19,27 +18,26 @@ mongo_db = MongoEngine(app)
 # flask assets - styling
 assets = Environment(app)
 assets.url = app.static_url_path
-scss = Bundle('index.scss', filters='pyscss', output='all.css')
-assets.register('scss_all', scss)
+scss = Bundle("index.scss", filters="pyscss", output="all.css")
+assets.register("scss_all", scss)
 
 
 # models
 class CovidUS(me.Document):
-    meta = {
-        'collection': 'covid_us'
-    }
+    meta = {"collection": "covid_us"}
     date = me.StringField(required=True, unique=True)
     cases = me.IntField(required=True, min_value=0)
     deaths = me.IntField(required=True, min_value=0)
 
 
 # define app routes
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/covid')
-@app.route('/covid/<date>')
+
+@app.route("/covid")
+@app.route("/covid/<date>")
 def covid_us(date=None):
     print(date, type(date))
     if date:
